66cfa598cbfc60070b0f7205d9f25e6ab1fcd4d1
Filmon <filmon.tesfamariam@hotmail.com>
2024-03-27 13:35:29 +0100

    header line done

--- citycab/src/component/header.js
+++ citycab/src/component/header.js
@@ -3,12 +3,13 @@ import './header.css'; // Import the CSS file
 
 function Header() {
   return (
-    
+
     <header className="App-header">
       {/* <img src={logo} className="App-logo" alt="logo" /> */}
       <div className="App-header-text">
         CityCab
         
+        <hr></hr>
       </div>
       
     </header>
