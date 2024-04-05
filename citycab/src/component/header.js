6844ed2d0e97067b0ec152d50f3b773d9058c7fd
Ehsan Hussain <ehsan-1995@hotmail.com>
2024-04-05 14:27:41 +0300

    home navigator

--- citycab/src/component/header.js
+++ citycab/src/component/header.js
@@ -1,4 +1,3 @@
-// Header.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './header.css'; // Säkerställ att du har skapat och stilar din header.css
@@ -6,14 +5,13 @@ import './header.css'; // Säkerställ att du har skapat och stilar din header.c
 function Header() {
   return (
     <header className="app-header">
-      <h1 className="app-title">CityCab</h1>
+      <h1 className="app-title">
+        <Link to="/" className="nav-link">CityCab</Link> {/* Gör "CityCab" till en hem-länk */}
+      </h1>
       <nav className="header-nav">
-        {/* Använd 'Link' istället för 'a' för klient-sidig routing */}
         <Link to="/ride" className="nav-link">Ride</Link>
         <Link to="/drive" className="nav-link">Drive</Link>
         <Link to="/help" className="nav-link help-link">Help</Link>
-        {/* Lägg till länken till My Profile */}
-        <Link to="/myprofile" className="nav-link">My Profile</Link>
       </nav>
     </header>
   );
--- citycab/src/component/myprofile.js
+++ citycab/src/component/myprofile.js
@@ -1,42 +1,77 @@
-// I din myprofile.js fil i component-mappen
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import './myprofile.css'; // Ställ in dina stilregler här
+  import React, { useState, useEffect } from 'react';
+  import axios from 'axios';
+  import './myprofile.css';
 
-function MyProfile() {
-  const [profileData, setProfileData] = useState({
-    username: '',
-    details: {},
-    metrics: {},
-    status: {},
-    settings: {},
-  });
+  function MyProfile() {
+    const [profileData, setProfileData] = useState({
+      username: '',
+      favoriteColor: '',
+      subscriptionLevel: '',
+    });
+    const [profilePicture, setProfilePicture] = useState(null);
 
-  useEffect(() => {
-    async function fetchProfileData() {
-      try {
-        const response = await axios.get('https://api-g36q5boh2q-uc.a.run.app/api/user-profile');
-        setProfileData(response.data);
-      } catch (error) {
-        console.error('Error fetching profile data:', error);
+    const handleUsernameChange = (e) => {
+      setProfileData({ ...profileData, username: e.target.value });
+    };
+
+
+    const handleProfilePictureChange = (e) => {
+      if (e.target.files.length > 0) {
+        // Skapa en URL för den valda filen
+        const fileURL = URL.createObjectURL(e.target.files[0]);
+        setProfilePicture(fileURL);
       }
-    }
+    };
 
-    fetchProfileData();
-  }, []);
 
-  return (
-    <div className="profile-container">
-      <h1>My Profile</h1>
-      <div>
-        <div className="user-name">{profileData.username}</div>
-        <div className="user-info">Favorite Color: {profileData.details.favoriteColor}</div>
-        <div className="user-info">Subscription Level: {profileData.details.subscriptionLevel}</div>
-        {/* Render more user info as needed */}
+    return (
+      <div className="profile-container">
+        <h1>My Profile</h1>
+        <div>
+          {/* Profilbild */}
+          <div className="profile-picture">
+            <img src={profilePicture || 'https://imgv3.fotor.com/images/blog-richtext-image/10-profile-picture-ideas-to-make-you-stand-out.jpg'} alt="Profile" />
+            <input type="file" onChange={handleProfilePictureChange} />
+          </div>
+
+          {/* Användarnamn */}
+          <div className="user-info">
+            <label htmlFor="username">Användarnamn:</label>
+            <input
+              id="username"
+              type="text"
+              value={profileData.username}
+              onChange={handleUsernameChange}
+            />
+          </div>
+
+          {/* Färg */}
+          <div className="user-info">
+            <label htmlFor="favoriteColor">Email:</label>
+            <input
+              id="favoriteColor"
+              type="text"
+              value={profileData.favoriteColor}
+              onChange={(e) => setProfileData({ ...profileData, favoriteColor: e.target.value })}
+            />
+          </div>
+
+          {/* Prenumerationsnivå */}
+          <div className="user-info">
+            <label htmlFor="subscriptionLevel">telefon:</label>
+            <input
+              id="subscriptionLevel"
+              type="text"
+              value={profileData.subscriptionLevel}
+              onChange={(e) => setProfileData({ ...profileData, subscriptionLevel: e.target.value })}
+            />
+          </div>
+
+          {/* En knapp för att spara de nya uppgifterna kan läggas till här */}
+          <button className="profile-button">Uppdatera profil</button>
+        </div>
       </div>
-      {/* Add more elements to display other profile details */}
-    </div>
-  );
-}
+    );
+  }
 
-export default MyProfile;
+  export default MyProfile;
